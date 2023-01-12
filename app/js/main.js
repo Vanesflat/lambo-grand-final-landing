0dd9295956308794901dc5d0bdb577a5c3743029
Vanesflat <ptptcptptc@yandex.ru>
2023-01-12 12:51:56 +0700

    Created and stylized footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+// Слайдкер Swiper API
 const swiper = new Swiper('.swiper', {
   loop: true,
   slidesPerView: 2,
@@ -7,3 +8,15 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.blog__btn-prev',
   },
 });
+
+// Карта Google API
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
